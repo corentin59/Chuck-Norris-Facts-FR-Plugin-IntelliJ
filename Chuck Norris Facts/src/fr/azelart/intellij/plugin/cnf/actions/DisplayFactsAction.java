5a216bfb3a36b98bcb5409b8e75e9add21ab8b58
Corentin <corentin.azelart@gmail.com>
2013-09-14 15:29:47 +0200

    Change for CNF API, remove fortune

--- Chuck Norris Facts/src/fr/azelart/intellij/plugin/cnf/actions/DisplayFactsAction.java
+++ Chuck Norris Facts/src/fr/azelart/intellij/plugin/cnf/actions/DisplayFactsAction.java
@@ -8,8 +8,14 @@ import com.intellij.openapi.wm.WindowManager;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +31,7 @@ public class DisplayFactsAction extends AnAction {
     /**
      * Facts.
      */
-    private List<String> facts;
+    private JSONArray facts;
 
     /**
      * User performed action on check norris icon.
@@ -39,40 +45,43 @@ public class DisplayFactsAction extends AnAction {
             try {
                 pushMessage("Please wait...", actionEvent);
                 this.reloadFacts();
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 pushMessage("Unable to load facts... please check your network connection or try again later...", actionEvent);
+            } catch (final JSONException e) {
+                pushMessage("Unable to load facts... try again later...", actionEvent);
             }
         }
 
         // Set fact...
-        final int max = facts.size() - 1;
+        final int max = facts.length() - 1;
         final int number = (int)(Math.random() * (max-0)) + 0;
 
-        pushMessage(facts.get(number), actionEvent);
+        try {
+            final JSONObject fact = (JSONObject) facts.get(number);
+            pushMessage(StringEscapeUtils.unescapeHtml(fact.getString("fact")), actionEvent);
+        } catch (final JSONException je) {
+            pushMessage("No fact to display... try again later...", actionEvent);
+        }
     }
 
     /**
      * Reload facts.
-     * @throws java.io.IOException if error with http request.
+     * @throws IOException if error with http request.
+     * @throws JSONException if we can't decode JSON
      */
-    private void reloadFacts() throws IOException{
+    private void reloadFacts() throws IOException, JSONException {
         // Get facts...
         final HttpClient client = new HttpClient();
-        final GetMethod getMethod = new GetMethod("http://www.chucknorrisfacts.fr/fortunes/fortunes.txt");
+        final GetMethod getMethod = new GetMethod("http://www.chucknorrisfacts.fr/api/get?data=tri:alea:nb:50");
 
         final int statusCode = client.executeMethod(getMethod);
 
         // Good response.
         if(statusCode == HttpStatus.SC_OK) {
             final String factsTxt = getMethod.getResponseBodyAsString();
-
-            // Parsing facts...
-            if(factsTxt!=null) {
-                final String[] factsArray = factsTxt.split("%");
-                if(factsArray.length>=1) {
-                    facts = new ArrayList<String>(Arrays.asList(factsArray));
-                }
-            }
+            facts = new JSONArray(factsTxt);
+        } else {
+            throw new IOException("Server response != 200");
         }
     }
 
